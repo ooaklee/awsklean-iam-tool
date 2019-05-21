02fe672d7f85bd92d35c897853720724d20c62a7
Leon Silcott <lnsilcott@gmail.com>
2019-05-21 19:28:33 +0100

    Added shell of awsklean rewrite

---
+++ awsklean.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import boto3
+import botocore
+import sys
+import os
+
+
+# GLOBAL SCRIPT VARIABLES
+script_location = os.path.dirname(os.path.realpath(__file__))
+script_version = "0.0.1"
