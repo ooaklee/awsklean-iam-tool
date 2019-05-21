7be7c15637066470d100667b812f5b43ad28488f
Leon Silcott <lnsilcott@gmail.com>
2019-05-21 22:32:38 +0100

    Added new argument for notifying slack, created function that prepares correct request body (tested with https://webhook.site), updated import statements and comments

--- awsklean.py
+++ awsklean.py
@@ -6,6 +6,9 @@ import botocore
 import sys
 import os
 import argparse
+import ast
+import json
+import requests
 
 
 
@@ -14,6 +17,7 @@ script_location = os.path.dirname(os.path.realpath(__file__))
 script_version = "0.0.1"
 script_name = sys.argv[0].strip(".py")
 is_dry_run_mode_set = False
+is_notify_slack_mode_set = False
 
 
 def is_dry_run_active(state: bool) -> None:
@@ -30,7 +34,21 @@ def is_dry_run_active(state: bool) -> None:
     is_dry_run_mode_set = state
 
 
-def should_show_version(passed: bool):
+def is_notify_slack_active(state: bool) -> None:
+    """Checks to see if --slack-notify argument is passed to the script and sets global variable accordingly
+
+
+    :param state: Whether --slack-notify was passed as argument
+    :type state: bool
+
+    :returns: None
+    """
+    global is_notify_slack_mode_set
+
+    is_notify_slack_mode_set = state
+
+
+def should_show_version(passed: bool) -> None:
     """Prints the tool's version number to the terminal and exits tool
 
 
@@ -38,11 +56,39 @@ def should_show_version(passed: bool):
     :type passed: bool
 
     :returns: None
-    :rtype: None
     """
     if passed:
         print(f"{script_name} version {script_version}")
         exit()
+
+def send_to_slack_this(message: str) -> None:
+    """Sends message to specified webhook that's saved as an environment variable
+
+    :param message: Message to send to Slack channel
+    :type message: str
+    :raises: :class:`KeyError`: Cannot find environment variable for AWSKLEAN_SLACK_WEBHOOK
+
+
+    :returns: None
+    """
+    try:
+        slack_webhook = os.environ["AWSKLEAN_SLACK_WEBHOOK"]
+    except KeyError:
+        print("""Unable to detect an environmental variable with the name AWSKLEAN_SLACK_WEBHOOK, Please
+set this before passing the --notify-slack argument.
+        """)
+        exit()
+    
+    # Build valid dict containing message and configuration
+    configured_message_dict = ast.literal_eval(
+        f"""{{ "text": "{message}", "icon_url": "https://www.freeiconspng.com/uploads/black-key-symbol-icon-6.png", "username": "{script_name[:4].upper() + script_name[4:].lower()}"}}"""
+    )
+
+    # Send POST request to webhook containing message
+    requests.post(
+        slack_webhook,
+        data = json.dumps(configured_message_dict)
+    )
 
 
 if __name__ == "__main__":
@@ -61,10 +107,19 @@ if __name__ == "__main__":
         action="store_true"
     )
 
+    argument_parser.add_argument(
+        "--notify-slack",
+        help="Use to send notification(s) back to slack channel using webhook (Soon to be deprecated)",
+        action="store_true"
+    )
+
     args = argument_parser.parse_args()
 
-    # Check if version argument(s) passed
+    # Action if version argument(s) passed
     should_show_version(args.version)
 
-    # Check to see if dry-run passed
+    # Update global variable if dry-run passed
     is_dry_run_active(args.dry_run)
+
+    # Update global variable if notify-slack passed
+    is_notify_slack_active(args.notify_slack)
